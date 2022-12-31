472193cddec54aa6fdb54bfea4842afe7095e27d
Aliaksandr Fedukovich <1632637@mail.ru>
2022-12-31 21:55:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arr = [];
+    for (let i = 0; i < expr.length; i = i + 10) {
+        let str = expr.slice(i, i + 10);
+        str === '**********' ? arr.push(' ') : arr.push(convertInDots(str.slice(str.indexOf('1'))));
+    }
+    
+    const answerArr = [];
+    arr.forEach(item => answerArr.push(convertDotInLetter(item)));
+/*     console.log(answerArr); */
+    return answerArr.join('');
+
+
+}
+
+function convertDotInLetter(string) {
+    for(let key in MORSE_TABLE) {
+        if( key === string) return MORSE_TABLE[key];
+    }
+    return ' ';
+}
+
+
+function convertInDots(str) {
+    let answer = '';
+    for (let i = 0; i < str.length; i = i + 2) {
+        str.slice(i, i + 2) === "11" ? answer = answer + '-' : answer = answer + '.';
+    }
+    return answer;
 }
 
 module.exports = {
